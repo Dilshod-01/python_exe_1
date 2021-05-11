b897f910f88f454b891603ff71d8c4ab0ec9e2c2
Dilshod-01 <valiyevdilshod99@gmail.com>
2021-05-11 15:29:47 +0500

    exercise1

---
+++ beginning_1.py
@@ -0,0 +1,10 @@
+print("Hi!")
+name = input("Whats your name? ")
+
+print("nice to meet you", name)
+answer = input("are you boy? ")
+
+if answer=="Yes":
+    print("thats good to hear")
+else:
+    print("oh no, but its okay byeee")  
